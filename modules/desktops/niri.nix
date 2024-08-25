c498377da656b9f14d23b66eca15fbde7b2e773a
GitHver <hver.vef@gmail.com>
2024-08-25 20:51:07 +0000

    uesr

--- modules/desktops/niri.nix
+++ modules/desktops/niri.nix
@@ -15,9 +15,74 @@ in {
   config = mkIf cfg.enable {
     programs.niri.enable = true;
     
-    programs.niri.settings.binds = {
-      "XF86AudioRaiseVolume".action.spawn = ["wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1+"];
-      "XF86AudioLowerVolume".action.spawn = ["wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1-"];
+    programs.niri.settings = {
+      inputs = {
+        keyboard.xkb = {
+          layout  = "";
+          model   = "";
+          variant = "";
+          options = "";
+          rules   = "";
+        };
+      };
+      # outputs = {
+      #   "eDP-1" = {
+          
+      #   };
+      # };
+      cursor = {
+        size = 24;
+        theme = "default";
+      };
+      # layout.border = {
+        
+      # };
+      # layout.focus-ring = {
+        
+      # };
+      # layout = {
+        
+      # };
+      # animations = {
+        
+      # };
+      # window-rules = {
+        
+      # };
+      binds = with config.lib.niri.actions; {
+        # Audio
+        "XF86AudioRaiseVolume".action.spawn = [ "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1+" ];
+        "XF86AudioLowerVolume".action.spawn = [ "wpctl" "set-volume" "@DEFAULT_AUDIO_SINK@" "0.1-" ];
+        "XF86AudioMute".action.spawn        = [ "wpctl" "set-mute" "@DEFAULT_AUDIO_SINK@" "toggle" ];
+        "XF86AudioMicMute".action.spawn     = [ "wpctl" "set-mute" "@DEFAULT_AUDIO_SOURCE@" "toggle" ];
+        # Programs
+        "Mod+D".action.spawn = "fuzzel";
+        "Mod+T".action.spawn = "alacritty";
+        # Window navigation
+        "Mod+H".action = focus-column-left-or-last;
+        "Mod+J".action = focus-window-or-workspace-down;
+        "Mod+K".action = focus-window-or-workspace-up;
+        "Mod+L".action = focus-column-right-or-first;
+        # Window moving
+        "Mod+Ctrl+H".action = move-column-left;
+        "Mod+Ctrl+J".action = move-window-down-or-to-workspace-down;
+        "Mod+Ctrl+K".action = move-window-up-or-to-workspace-up;
+        "Mod+Ctrl+L".action = move-column-right;
+        # Column forming
+        "Mod+Y".action = consume-or-expel-window-left;
+        "Mod+O".action = consume-or-expel-window-right;
+        # Window resizing
+        "Mod+U".action = switch-preset-column-width;
+        "Mod+F".action = maximize-column;
+        "Mod+Shift+F".action = fullscreen-window;
+        # "Mod+Shift+H".action = 
+        # "Mod+Shift+J".action =
+        # "Mod+Shift+K".action =
+        # "Mod+Shift+L".action =
+        # Other
+        "Mod+Shift+E".action.quit.skip-confirmation = true;
+        "Mod+Q".action = close-window;
+      };
     };
   };
 }
