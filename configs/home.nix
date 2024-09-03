a482c8604f108bdf30717e642402fcce041fea89
GitHver? <hver.vef@gmail.com>
2024-09-03 16:17:56 +0000

    new-er

--- configs/home.nix
+++ configs/home.nix
@@ -28,8 +28,8 @@ in {
   #   dash-to-dock.enable = true;
   #   blur-my-shell.enable = true;
   # };
-  # niri.enable = true;
-  # wayland-core.enable = true;
+  niri.enable = true;
+  wayland-core.enable = true;
   # wbg.enable = true;
   # programs.eww = {
   #   enable = true;
